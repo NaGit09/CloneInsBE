b1cb64e8e979a3e31bf852b439f15d78632bd2aa
NaGit09 <nhutanhtran806@gmail.com>
2025-02-07 21:46:38 +0700

    add  : add query mysql

--- src/main/java/org/example/cloneins/Database/Database.java
+++
@@ -1,7 +0,0 @@
-package org.example.cloneins.Database;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class Database {
-}
--- src/main/java/org/example/cloneins/Repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.cloneins.Repository;
-
-import org.example.cloneins.Model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product , Long > {
-}
